1682219c135a1d7aa9e51cd101be5e36e2fa08de
Darina Tkachenko <potterpeppi@gmail.com>
2023-10-29 09:03:02 +0200

    rectangles

---
+++ test.js
@@ -0,0 +1,6 @@
+const savebtn = document.getElementById("savebtn")
+savebtn.addEventListener("click", (_) => {
+    const busyTime = document.getElementById(busyTime);
+    const busyTimeValue = busyTime.value;
+
+});

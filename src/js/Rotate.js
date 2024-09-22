df8309eb351449eb4c20d6e90e17e6dddd8cc9d5
Stanislav Bashkyrtsev <stanislav.bashkyrtsev@elsci.io>
2024-09-22 11:21:03 +0300

    Document Rotate functions

--- src/js/Rotate.js
+++ src/js/Rotate.js
@@ -2,10 +2,24 @@ import StaticMath from "./StaticMath.js";
 
 export default class Rotate {
 
+    /**
+     * @param {Vector} vector
+     * @param {number} ax
+     * @param {number} ay
+     * @param {number} az
+     * @return {Vector}
+     */
     static rotateVec(vector, ax, ay, az) {
         return Rotate.getRotationMatrix(ax, ay, az).vectorMultiply(vector);
     }
 
+    /**
+     * @param {Matrix[]}arrayOfMatrices
+     * @param {number} angleX
+     * @param {number} angleY
+     * @param {number} angleZ
+     * @return {*[]}
+     */
     static rotateArrayOfMatrices(arrayOfMatrices, angleX = 0, angleY = 0, angleZ = 0) {
         const result = [];
         for (let m = 0; m < arrayOfMatrices.length; m++) {
@@ -18,6 +32,12 @@ export default class Rotate {
         return result;
     }
 
+    /**
+     * @param {number} angleX
+     * @param {number} angleY
+     * @param {number} angleZ
+     * @return {Matrix}
+     */
     static getRotationMatrix(angleX, angleY, angleZ) {
         const matrixX = StaticMath.getXMatrix(angleX)
         const matrixY = StaticMath.getYMatrix(angleY)
